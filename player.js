8bdc2d0da51d9712b812066860867fe48e542399
Pavel Egorov <xoposhiy@mail.ru>
2016-04-09 12:31:45 +0500

    v1

--- player.js
+++ player.js
@@ -4,7 +4,7 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-    bet(0);
+    bet(30);
   },
 
   showdown: function(game_state) {

a33c17525efe1283e5eec100248762300f2cebfd
Pavel Egorov <xoposhiy@mail.ru>
2016-04-09 15:33:33 +0500

    new getHandScore

--- Cards.js
+++ Cards.js
@@ -11,9 +11,14 @@ function getHandScore(hand){
     var c2 = hand[1];
     var score = scoreRank(c1.rank) + scoreRank(c2.rank);
     if (c1.rank == c2.rank)
-        score += 10; 
-    if (c1.suit == c2.suit)
-        score += 4;
+        score += 10;
+    var diff = rankIndex(c1.rank)-rankIndex(c2.rank); 
+    if (diff == 1 || diff == -1)
+        score += 3;
+    if (diff == 2 || diff == -2)
+        score += 2;
+    if (diff == 3 || diff == -3)
+        score += 1;
     return score; 
 }
 
@@ -32,6 +37,24 @@ function scoreRank(rank){
         'Q': 13,
         'K': 14,
         'A': 16
+    };
+    return map[rank];
+}
+function rankIndex(rank){
+    var map = {
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        '10': 10,
+        'J': 11,
+        'Q': 12,
+        'K': 13,
+        'A': 14
     };
     return map[rank];
 }

7f937bf90648a5b641c5dfe3cc49bae493186c1d
Roman Paradeev <sameoldmadness@gmail.com>
2016-04-09 17:11:30 +0500

    restore saveState

--- player.js
+++ player.js
@@ -1,13 +1,14 @@
 var Cards = require("./Cards.js");
 var Better = require("./Better.js");
 var state = require('./lib/saveState');
+var saveState = state.saveState;
 
 module.exports = {
 
   VERSION: "HA!",
 
   bet_request: function(state, bet) {
-    // state.saveState(state, 'rounds');
+    saveState(state, 'rounds');
 
     var value = Better.getBet(state);
     bet(value);

624eab45221239947c8da695e56437b11b62d3ce
Roman Paradeev <sameoldmadness@gmail.com>
2016-04-09 16:50:08 +0500

    log rounds

--- player.js
+++ player.js
@@ -7,12 +7,14 @@ module.exports = {
   VERSION: "3",
 
   bet_request: function(state, bet) {
+    state.saveState(game_state, 'rounds');
+
     var value = Better.getBet(state);
     bet(value);
   },
 
   showdown: function(game_state) {
-    state.saveState(game_state);
+    state.saveState(game_state, 'states');
   },
 
   getState: state.loadState

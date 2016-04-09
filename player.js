e3d7f55e1082beb3d3df3504f673f93470a76998
Pavel Egorov <xoposhiy@mail.ru>
2016-04-09 14:02:32 +0500

    Cards.getHandScore

--- player.js
+++ player.js
@@ -1,37 +1,35 @@
 var PokerEvaluator = require("poker-evaluator");
-const state = require('./lib/saveState');
-var Converter = require("./convertcards.js");
+var Cards = require("./Cards.js");
 
 // hand as in game state
-var scoreHand = function(hand) {
-  var h = Converter.convertHand(hand);
-  var result = PokerEvaluator.evalHand(h);
-  return result.handRank;
+var getBet = function(hand, myStack, buy_in) {
+  var score = Cards.getHandScore(hand);
+  if (score > 30) {
+      console.log(hand + " - allin");
+      return 1000000;
+  }
+  if (score > 25) {
+      console.log(hand + " - stack/4");
+      return myStack/2;
+    }
+  if (score > 14 && buy_in < myStack/4){
+    console.log(hand + " - ok");
+    return buy_in;  
+  } 
+  return 0; 
 };
 
 module.exports = {
 
   VERSION: "checker (no all in)",
 
-  bet_request: function(game_state, bet) {
-    var me = game_state.players[game_state.in_action];
-	if (game_state.current_buy_in > 500)
-		bet(0);
-	else
-		bet(game_state.current_buy_in - game_state.players[game_state.in_action].bet);
+  bet_request: function(state, bet) {
+    var me = state.players[state.in_action];
+    var value = getBet(me.hole_cards, me.stack, state.current_buy_in);
+    bet(value);
   },
 
   showdown: function(game_state) {
     saveState(game_state);
   }
 };
-
-function test(){
-    console.log(scoreHand([
-    {rank:"10", suit:"spades"}, 
-    {rank:"2", suit:"spades"},
-    {rank:"2", suit:"diamonds"}
-    ]));
-}
-
-test();

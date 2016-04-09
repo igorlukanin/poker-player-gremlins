5168cf9f7ff0bab6630d72e079a1e9fc257c6c03
Pavel Egorov <xoposhiy@mail.ru>
2016-04-09 12:47:50 +0500

    no allin

--- player.js
+++ player.js
@@ -1,10 +1,13 @@
 
 module.exports = {
 
-  VERSION: "checker 1",
+  VERSION: "checker (no all in)",
 
   bet_request: function(game_state, bet) {
-    bet(game_state.current_buy_in - game_state.players[game_state.in_action].bet);
+	if (game_state.current_buy_in > 500)
+		bet(0);
+	else
+		bet(game_state.current_buy_in - game_state.players[game_state.in_action].bet);
   },
 
   showdown: function(game_state) {
